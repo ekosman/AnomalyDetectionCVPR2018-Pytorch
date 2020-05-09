84033a7d8b183c7a464a3fbdad25249e5cb586d1
Eitan <eitan.kosman@gmail.com>
2020-05-09 11:55:57 +0300

    refactor data_loader

--- data_loader.py
+++ data_loader.py
@@ -5,8 +5,7 @@ import torch.utils.data as data
 from torchvision.datasets.video_utils import VideoClips
 
 
-class VideoIterTrain(data.Dataset):
-
+class VideoIter(data.Dataset):
     def __init__(self,
                  dataset_path,
                  annotation_path,
@@ -14,15 +13,10 @@ class VideoIterTrain(data.Dataset):
                  frame_stride,
                  video_transform=None,
                  name="<NO_NAME>",
-                 return_item_subpath=False,
                  shuffle_list_seed=None):
-        super(VideoIterTrain, self).__init__()
-
-        self.force_color = True
         self.dataset_path = dataset_path
         self.frames_stride = frame_stride
         self.video_transform = video_transform
-        self.return_item_subpath = return_item_subpath
         self.rng = np.random.RandomState(shuffle_list_seed if shuffle_list_seed else 0)
         # load video list
         self.video_list = self._get_video_list(dataset_path=self.dataset_path, annotation_path=annotation_path)
@@ -31,6 +25,35 @@ class VideoIterTrain(data.Dataset):
                                       clip_length_in_frames=self.total_clip_length_in_frames,
                                       frames_between_clips=self.total_clip_length_in_frames)
         logging.info("VideoIter:: iterator initialized (phase: '{:s}', num: {:d})".format(name, len(self.video_list)))
+
+    @staticmethod
+    def _get_video_list(dataset_path, annotation_path):
+        assert os.path.exists(dataset_path)  # , "VideoIter:: failed to locate: `{}'".format(dataset_path)
+        assert os.path.exists(annotation_path)  # , "VideoIter:: failed to locate: `{}'".format(annotation_path)
+        vid_list = None
+        with open(annotation_path, 'r') as f:
+            lines = f.read().splitlines(keepends=False)
+            vid_list = [os.path.join(dataset_path, line.split()[0]) for line in lines]
+
+        if vid_list is None:
+            raise RuntimeError("Unable to parse annotations!")
+
+        return vid_list
+
+    def __len__(self):
+        return len(self.video_list)
+
+    def __getitem__(self, index):
+        succ = False
+        while not succ:
+            try:
+                clip_input, label, sampled_idx, dir, file = self.getitem_from_raw_video(index)
+                succ = True
+            except Exception as e:
+                index = self.rng.choice(range(0, self.__len__()))
+                logging.warning("VideoIter:: ERROR!! (Force using another index:\n{})\n{}".format(index, e))
+
+        return clip_input, label, sampled_idx, dir, file
 
     def getitem_from_raw_video(self, idx):
         # get current video info
@@ -42,112 +65,10 @@ class VideoIterTrain(data.Dataset):
         if self.video_transform is not None:
             video = self.video_transform(video)
 
-        if "Normal" not in video_path:
-            label = 1
-        else:
-            label = 0
+        label = 0 if "Normal" in video_path else 1
 
         dir, file = video_path.split(os.sep)[-2:]
         file = file.split('.')[0]
 
         return video, label, clip_idx, dir, file
 
-    def __getitem__(self, index):
-        succ = False
-        while not succ:
-            try:
-                clip_input, label, sampled_idx, dir, file = self.getitem_from_raw_video(index)
-                succ = True
-            except Exception as e:
-                index = self.rng.choice(range(0, self.__len__()))
-                logging.warning("VideoIter:: ERROR!! (Force using another index:\n{})\n{}".format(index, e))
-
-        return clip_input, label, sampled_idx, dir, file
-
-    def __len__(self):
-        return len(self.video_list)
-
-    def _get_video_list(self, dataset_path, annotation_path):
-
-        assert os.path.exists(dataset_path)  # , "VideoIter:: failed to locate: `{}'".format(dataset_path)
-        assert os.path.exists(annotation_path)  # , "VideoIter:: failed to locate: `{}'".format(annotation_path)
-        vid_list = []
-        with open(annotation_path, 'r') as f:
-            for line in f:
-                items = line.split()
-
-                path = os.path.join(dataset_path, items[0])
-                vid_list.append(path.strip('\n'))
-        return vid_list
-
-
-class VideoIterVal(data.Dataset):
-
-    def __init__(self,
-                 dataset_path,
-                 annotation_path,
-                 clip_length,
-                 frame_stride,
-                 video_transform=None,
-                 name="<NO_NAME>",
-                 return_item_subpath=False,
-                 shuffle_list_seed=None):
-        super(VideoIterVal, self).__init__()
-        # load params
-        self.frames_stride = frame_stride
-        self.dataset_path = dataset_path
-        self.video_transform = video_transform
-        self.return_item_subpath = return_item_subpath
-        self.rng = np.random.RandomState(shuffle_list_seed if shuffle_list_seed else 0)
-        # load video list
-        self.video_list = self._get_video_list(dataset_path=self.dataset_path, annotation_path=annotation_path)
-        self.total_clip_length_in_frames = clip_length * frame_stride
-        self.video_clips = VideoClips(video_paths=self.video_list,
-                                      clip_length_in_frames=self.total_clip_length_in_frames,
-                                      frames_between_clips=self.total_clip_length_in_frames)
-        logging.info("VideoIter:: iterator initialized (phase: '{:s}', num: {:d})".format(name, len(self.video_list)))
-
-    def getitem_from_raw_video(self, idx):
-        # get current video info
-        video, _, _, _ = self.video_clips.get_clip(idx)
-        video_idx, clip_idx = self.video_clips.get_clip_location(idx)
-        video_path = self.video_clips.video_paths[video_idx]
-        if self.video_transform is not None:
-            video = self.video_transform(video)
-
-        if "Normal" not in video_path:
-            label = 1
-        else:
-            label = 0
-
-        dir, file = video_path.split(os.sep)[-2:]
-        file = file.split('.')[0]
-        in_clip_frames = list(range(0, self.total_clip_length_in_frames, self.frames_stride))
-        return video[in_clip_frames], label, clip_idx, dir, file
-
-    def __getitem__(self, index):
-        succ = False
-        while not succ:
-            try:
-                clip_input, label, sampled_idx, dir, file = self.getitem_from_raw_video(index)
-                succ = True
-            except Exception as e:
-                index = self.rng.choice(range(0, self.__len__()))
-                logging.warning("VideoIter:: ERROR!! (Force using another index:\n{})\n{}".format(index, e))
-
-        return clip_input, label, sampled_idx, dir, file
-
-    def __len__(self):
-        return len(self.video_list)
-
-    def _get_video_list(self, dataset_path, annotation_path):
-        assert os.path.exists(dataset_path)  # , "VideoIter:: failed to locate: `{}'".format(dataset_path)
-        assert os.path.exists(annotation_path)  # , "VideoIter:: failed to locate: `{}'".format(annotation_path)
-        v_id = 0
-        vid_list = []
-        with open(annotation_path, 'r') as f:
-            for line in f:
-                items = line.split()
-                path = os.path.join(dataset_path, items[0])
-                vid_list.append(path.strip('\n'))
-        return vid_list
--- feature_extractor.py
+++ feature_extractor.py
@@ -1,70 +1,67 @@
 import argparse
 import os
 from os import path, mkdir
-
 import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
 from tqdm import tqdm
-
+from data_loader import VideoIter
 from network.c3d import C3D
-from data_loader import VideoIterTrain
 from utils.utils import set_logger, build_transforms
 
 parser = argparse.ArgumentParser(description="PyTorch Video Classification Parser")
 # debug
 parser.add_argument('--debug-mode', type=bool, default=True,
-                    help="print all setting for debugging.")
+					help="print all setting for debugging.")
 # io
 parser.add_argument('--dataset_path', default='../kinetics2/kinetics2/AnomalyDetection',
-                    help="path to dataset")
+					help="path to dataset")
 parser.add_argument('--annotation_path', default="Train_Annotation.txt",
-                    help="path to train annotation")
+					help="path to train annotation")
 parser.add_argument('--annotation_path_test', default="Test_Annotation.txt",
-                    help="path to test annotation")
+					help="path to test annotation")
 parser.add_argument('--clip-length', type=int, default=16,
-                    help="define the length of each input sample.")
+					help="define the length of each input sample.")
 parser.add_argument('--train-frame-interval', type=int, default=2,
-                    help="define the sampling interval between frames.")
+					help="define the sampling interval between frames.")
 parser.add_argument('--val-frame-interval', type=int, default=2,
-                    help="define the sampling interval between frames.")
+					help="define the sampling interval between frames.")
 parser.add_argument('--task-name', type=str, default='',
-                    help="name of current task, leave it empty for using folder name")
+					help="name of current task, leave it empty for using folder name")
 parser.add_argument('--model-dir', type=str, default="./exps/models",
-                    help="set logging file.")
+					help="set logging file.")
 parser.add_argument('--log-file', type=str, default="",
-                    help="set logging file.")
+					help="set logging file.")
 
 # device
 parser.add_argument('--gpus', type=str, default="0,1,2,3",
-                    help="define gpu id")
+					help="define gpu id")
 parser.add_argument('--pretrained_3d', type=str,
-                    default='',
-                    help="load default 3D pretrained model.")
+					default='',
+					help="load default 3D pretrained model.")
 parser.add_argument('--resume-epoch', type=int, default=-1,
-                    help="resume train")
+					help="resume train")
 # optimization
 parser.add_argument('--fine-tune', type=bool, default=True, help="apply different learning rate for different layers")
 parser.add_argument('--batch-size', type=int, default=8,
-                    help="batch size")
+					help="batch size")
 parser.add_argument('--lr-base', type=float, default=0.005,
-                    help="learning rate")
+					help="learning rate")
 parser.add_argument('--lr_mult_old_layers', type=float, default=0.2,
-                    help="learning rate")
+					help="learning rate")
 parser.add_argument('--weight_decay', type=float, default=0.0001,
-                    help="weight_decay")
+					help="weight_decay")
 parser.add_argument('--lr-steps', type=list, default=[int(1e4 * x) for x in [5, 10, 15]],
-                    ##In spli2 we can make the step a bit later
-                    help="number of samples to pass before changing learning rate")  # 1e6 million
+					help="number of samples to pass before changing learning rate")  # 1e6 million
 
 parser.add_argument('--lr-factor', type=float, default=0.1,
-                    help="reduce the learning with factor")
+					help="reduce the learning with factor")
 parser.add_argument('--save-frequency', type=float, default=1,
-                    help="save once after N epochs")
+					help="save once after N epochs")
 parser.add_argument('--end-epoch', type=int, default=100,
-                    help="maxmium number of training epoch")
+					help="maxmium number of training epoch")
 parser.add_argument('--random-seed', type=int, default=1,
-                    help='random seed (default: 1)')
+					help='random seed (default: 1)')
 
 features_dir = r"features2"
 
@@ -74,142 +71,138 @@ current_data = None
 
 
 class FeaturesWriter:
-    def __init__(self, chunk_size=16):
-        self.path = None
-        self.dir = None
-        self.data = None
-        self.chunk_size = chunk_size
+	def __init__(self, chunk_size=16):
+		self.path = None
+		self.dir = None
+		self.data = None
+		self.chunk_size = chunk_size
 
-    def _init_video(self, video_name, dir):
-        self.path = path.join(dir, f"{video_name}.txt")
-        self.dir = dir
-        self.data = dict()
+	def _init_video(self, video_name, dir):
+		self.path = path.join(dir, f"{video_name}.txt")
+		self.dir = dir
+		self.data = dict()
 
-    def has_video(self):
-        return self.data is not None
+	def has_video(self):
+		return self.data is not None
 
-    def dump(self):
-        print(f'Dumping {self.path}')
-        if not path.exists(self.dir):
-            os.mkdir(self.dir)
+	def dump(self):
+		print(f'Dumping {self.path}')
+		if not path.exists(self.dir):
+			os.mkdir(self.dir)
 
-        features = np.array([self.data[key] for key in sorted(self.data)])
-        features = features / np.expand_dims(np.linalg.norm(features, ord=2, axis=-1), axis=-1)
-        padding_count = int(32 * np.ceil(features.shape[0] / 32) - features.shape[0])
-        features = torch.from_numpy(np.vstack([features, torch.zeros(padding_count, 4096)]))
-        segments = torch.stack(torch.chunk(features, chunks=32, dim=0))
-        avg_segments = segments.mean(dim=-2).numpy()
-        with open(self.path, 'w') as fp:
-            for d in avg_segments:
-                d = [str(x) for x in d]
-                fp.write(' '.join(d) + '\n')
+		features = np.array([self.data[key] for key in sorted(self.data)])
+		features = features / np.expand_dims(np.linalg.norm(features, ord=2, axis=-1), axis=-1)
+		padding_count = int(32 * np.ceil(features.shape[0] / 32) - features.shape[0])
+		features = torch.from_numpy(np.vstack([features, torch.zeros(padding_count, 4096)]))
+		segments = torch.stack(torch.chunk(features, chunks=32, dim=0))
+		avg_segments = segments.mean(dim=-2).numpy()
+		with open(self.path, 'w') as fp:
+			for d in avg_segments:
+				d = [str(x) for x in d]
+				fp.write(' '.join(d) + '\n')
 
-    def _is_new_video(self, video_name, dir):
-        new_path = path.join(dir, f"{video_name}.txt")
-        if self.path != new_path and self.path is not None:
-            return True
+	def _is_new_video(self, video_name, dir):
+		new_path = path.join(dir, f"{video_name}.txt")
+		if self.path != new_path and self.path is not None:
+			return True
 
-        return False
+		return False
 
-    def store(self, feature, start_frame):
-        self.data[start_frame // self.chunk_size] = list(feature.cpu().numpy())
+	def store(self, feature, start_frame):
+		self.data[start_frame // self.chunk_size] = list(feature.cpu().numpy())
 
-    def write(self, feature, video_name, start_frame, dir):
-        if not self.has_video():
-            self._init_video(video_name, dir)
+	def write(self, feature, video_name, start_frame, dir):
+		if not self.has_video():
+			self._init_video(video_name, dir)
 
-        if self._is_new_video(video_name, dir):
-            self.dump()
-            self._init_video(video_name, dir)
+		if self._is_new_video(video_name, dir):
+			self.dump()
+			self._init_video(video_name, dir)
 
-        self.store(feature, start_frame)
+		self.store(feature, start_frame)
 
 
 def read_features(video_name, dir):
-    file_path = f"{video_name}.txt"
-    file_path = path.join(dir, file_path)
-    if not path.exists(file_path):
-        raise Exception(f"Feature doesn't exist: {file_path}")
-    features = None
-    with open(file_path, 'r') as fp:
-        data = fp.read().splitlines(keepends=False)
-        features = np.zeros((len(data), 4096))
-        for i, line in enumerate(data):
-            features[i, :] = [float(x) for x in line.split(' ')]
+	file_path = f"{video_name}.txt"
+	file_path = path.join(dir, file_path)
+	if not path.exists(file_path):
+		raise Exception(f"Feature doesn't exist: {file_path}")
+	features = None
+	with open(file_path, 'r') as fp:
+		data = fp.read().splitlines(keepends=False)
+		features = np.zeros((len(data), 4096))
+		for i, line in enumerate(data):
+			features[i, :] = [float(x) for x in line.split(' ')]
 
-    return torch.from_numpy(features)
+	return torch.from_numpy(features)
 
 
 def main():
-    device = torch.device("cuda" if torch.cuda.is_available()
-                          else "cpu")
+	device = torch.device("cuda" if torch.cuda.is_available()
+						else "cpu")
 
-    args = parser.parse_args()
-    set_logger(log_file=args.log_file, debug_mode=args.debug_mode)
+	args = parser.parse_args()
+	set_logger(log_file=args.log_file, debug_mode=args.debug_mode)
 
-    torch.manual_seed(args.random_seed)
-    torch.cuda.manual_seed(args.random_seed)
-    cudnn.benchmark = True
+	torch.manual_seed(args.random_seed)
+	torch.cuda.manual_seed(args.random_seed)
+	cudnn.benchmark = True
 
-    train_loader = VideoIterTrain(dataset_path=args.dataset_path,
-                                  annotation_path=args.annotation_path,
-                                  clip_length=args.clip_length,
-                                  frame_stride=args.train_frame_interval,
-                                  video_transform=build_transforms(),
-                                  name='train',
-                                  return_item_subpath=False,
-                                  )
+	train_loader = VideoIter(dataset_path=args.dataset_path,
+							annotation_path=args.annotation_path,
+							clip_length=args.clip_length,
+							frame_stride=args.train_frame_interval,
+							video_transform=build_transforms(),
+							name='train')
 
-    train_iter = torch.utils.data.DataLoader(train_loader,
-                                             batch_size=args.batch_size,
-                                             shuffle=False,
-                                             num_workers=32,  # 4, # change this part accordingly
-                                             pin_memory=True)
+	train_iter = torch.utils.data.DataLoader(train_loader,
+											batch_size=args.batch_size,
+											shuffle=False,
+											num_workers=32,  # 4, # change this part accordingly
+											pin_memory=True)
 
-    val_loader = VideoIterTrain(dataset_path=args.dataset_path,
-                                annotation_path=args.annotation_path_test,
-                                clip_length=args.clip_length,
-                                frame_stride=args.val_frame_interval,
-                                video_transform=build_transforms(),
-                                name='val',
-                                return_item_subpath=False,
-                                )
+	val_loader = VideoIter(dataset_path=args.dataset_path,
+						annotation_path=args.annotation_path_test,
+						clip_length=args.clip_length,
+						frame_stride=args.val_frame_interval,
+						video_transform=build_transforms(),
+						name='val')
 
-    val_iter = torch.utils.data.DataLoader(val_loader,
-                                           batch_size=args.batch_size,
-                                           shuffle=False,
-                                           num_workers=32,  # 4, # change this part accordingly
-                                           pin_memory=True)
+	val_iter = torch.utils.data.DataLoader(val_loader,
+										batch_size=args.batch_size,
+										shuffle=False,
+										num_workers=32,  # 4, # change this part accordingly
+										pin_memory=True)
 
-    network = C3D(pretrained=args.pretrained_3d)
-    network.to(device)
+	network = C3D(pretrained=args.pretrained_3d)
+	network.to(device)
 
-    if not path.exists(features_dir):
-        mkdir(features_dir)
+	if not path.exists(features_dir):
+		mkdir(features_dir)
 
-    features_writer = FeaturesWriter()
+	features_writer = FeaturesWriter()
 
-    for i_batch, (data, target, sampled_idx, dirs, vid_names) in tqdm(enumerate(train_iter)):
-        with torch.no_grad():
-            outputs = network(data.cuda())
+	for i_batch, (data, target, sampled_idx, dirs, vid_names) in tqdm(enumerate(train_iter)):
+		with torch.no_grad():
+			outputs = network(data.cuda())
 
-            for i, (dir, vid_name, start_frame) in enumerate(zip(dirs, vid_names, sampled_idx.cpu().numpy())):
-                dir = path.join(features_dir, dir)
-                features_writer.write(feature=outputs[i], video_name=vid_name, start_frame=start_frame, dir=dir)
+			for i, (dir, vid_name, start_frame) in enumerate(zip(dirs, vid_names, sampled_idx.cpu().numpy())):
+				dir = path.join(features_dir, dir)
+				features_writer.write(feature=outputs[i], video_name=vid_name, start_frame=start_frame, dir=dir)
 
-    features_writer.dump()
+	features_writer.dump()
 
-    features_writer = FeaturesWriter()
-    for i_batch, (data, target, sampled_idx, dirs, vid_names) in tqdm(enumerate(val_iter)):
-        with torch.no_grad():
-            outputs = network(data.cuda())
+	features_writer = FeaturesWriter()
+	for i_batch, (data, target, sampled_idx, dirs, vid_names) in tqdm(enumerate(val_iter)):
+		with torch.no_grad():
+			outputs = network(data.cuda())
 
-            for i, (dir, vid_name, start_frame) in enumerate(zip(dirs, vid_names, sampled_idx.cpu().numpy())):
-                dir = path.join(features_dir, dir)
-                features_writer.write(feature=outputs[i], video_name=vid_name, start_frame=start_frame, dir=dir)
+			for i, (dir, vid_name, start_frame) in enumerate(zip(dirs, vid_names, sampled_idx.cpu().numpy())):
+				dir = path.join(features_dir, dir)
+				features_writer.write(feature=outputs[i], video_name=vid_name, start_frame=start_frame, dir=dir)
 
-    features_writer.dump()
+	features_writer.dump()
 
 
 if __name__ == "__main__":
-    main()
+	main()
