1f053c19ae4311239fd620eccac5d5ce76fdd1d4
Eitan <eitan.kosman@gmail.com>
2020-10-09 00:01:11 +0300

    REMOVE tqdm from feature extraction

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,9 @@
 import logging
 import os
+import pickle
+
 import numpy as np
+import path
 import torch.utils.data as data
 from torchvision.datasets.video_utils import VideoClips
 
@@ -24,9 +27,18 @@ class VideoIter(data.Dataset):
         self.return_label = return_label
 
         # data loading
-        self.video_clips = VideoClips(video_paths=self.video_list,
-                                      clip_length_in_frames=self.total_clip_length_in_frames,
-                                      frames_between_clips=self.total_clip_length_in_frames,)
+        video_clips_path = r'video_clips.file'
+        if path.exists(video_clips_path):
+            with open(video_clips_path, 'rb') as fp:
+                self.video_clips = pickle.load(fp)
+
+        else:
+            self.video_clips = VideoClips(video_paths=self.video_list,
+                                          clip_length_in_frames=self.total_clip_length_in_frames,
+                                          frames_between_clips=self.total_clip_length_in_frames,)
+
+            with open(video_clips_path, 'wb') as fp:
+                pickle.dump(self.video_clips, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
     @property
     def video_count(self):
