6da250fdb3bb5d8c4ffea9ad098524ae19546f97
Eitan <eitan.kosman@gmail.com>
2020-11-20 14:04:49 +0200

    ADD save model arg

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -7,6 +7,7 @@ class TensorboardCallback(AbstractCallback):
 
     def on_epoch_end(self, training_context):
         print(training_context)
-        self.tb_writer.add_scalars('Train loss',
-                                   training_context['current_loss'],
-                                   training_context['_current_epoch'])
+        if training_context['current_loss'] is not None:
+            self.tb_writer.add_scalars('Train loss',
+                                       training_context['current_loss'],
+                                       training_context['_current_epoch'])
