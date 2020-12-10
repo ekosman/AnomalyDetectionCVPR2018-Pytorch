5f3b3f7f84bd0723c9a1e31db129f00297640d7c
Eitan <eitan.kosman@gmail.com>
2020-12-10 14:08:49 +0200

    ADD pretrained mfnet for ucf101

--- data_loader.py
+++ data_loader.py
@@ -30,6 +30,9 @@ class VideoIter(data.Dataset):
                                       clip_length_in_frames=self.total_clip_length_in_frames,
                                       frames_between_clips=self.total_clip_length_in_frames,)
 
+        with open('video_clips.file', 'wb') as fp:
+            pickle.dump(self.video_clips, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
     @property
     def video_count(self):
         return len(self.video_list)
